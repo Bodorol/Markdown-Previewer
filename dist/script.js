9b9a8b56c6d7b5088f87980914993fe774d730d8
Bodorol <brenner.lattin@gmail.com>
2020-10-10 20:07:57 -0600

    Initial commit

---
+++ dist/script.js
@@ -0,0 +1,83 @@
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}class Editor extends React.Component {
+  constructor(props) {
+    super(props);_defineProperty(this, "defaultVal",
+
+
+
+
+
+
+
+
+
+
+
+
+    `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`);this.state = { text: this.defaultVal };this.changeText = this.changeText.bind(this);}changeText(event) {this.setState({ text: event.target.value });}
+
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement("textarea", { id: "editor", onChange: this.changeText, rows: "35", cols: "200", defaultValue: this.defaultVal }),
+      React.createElement(Previewer, { text: this.state.text })));
+
+
+  }}
+
+
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      React.createElement("div", { id: "preview", dangerouslySetInnerHTML: { __html: marked(this.props.text) } }));
+
+  }}
+
+
+ReactDOM.render(React.createElement(Editor, null), document.getElementById("previewer"));
